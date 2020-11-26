d13bd518a1db6337c1286c549b8e1c458bdc8212
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2020-11-26 21:07:05 +0530

    Setup theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, lightTheme } from "./theme";
+
 import Home from "./views/Home";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
   return (
     <main>
-      <Home />
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyles />
+        <Home />
+      </ThemeProvider>
     </main>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from "styled-components";
+
+const colors = {
+  violet: "#5964e0",
+  lightViolet: "#939bf4",
+  veryDarkBlue: "#19202D",
+  midnight: "#121721",
+  white: "#fff",
+  lightGrey: "#f4f6f8",
+  grey: "#9daec2",
+  darkGrey: "#6e8098",
+};
+
+export const lightTheme = {
+  primary: colors.violet,
+  primaryLight: colors.lightViolet,
+  body: colors.lightGrey,
+  text: colors.darkGrey,
+  heading: colors.veryDarkBlue,
+  white: colors.white,
+  card: colors.white,
+};
+
+export const darkTheme = {
+  primary: colors.violet,
+  primaryLight: colors.lightViolet,
+  body: colors.lightGrey,
+  text: colors.darkGrey,
+  heading: colors.white,
+  white: colors.white,
+  card: colors.veryDarkBlue,
+};
+
+export const GlobalStyles = createGlobalStyle<{ theme: typeof lightTheme }>`
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    font-family: 'Kumbh Sans', sans-serif;
+  }
+`;
